1f7addc639460c197d19951c476da43a074e16ed
= <=>
2019-07-06 13:33:46 +0300

    first commit

---
+++ src/main/java/com/sda/parkingTicket/ParkingTicketApplication.java
@@ -0,0 +1,13 @@
+package com.sda.parkingTicket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingTicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sda/parkingTicket/ParkingTicketApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sda.parkingTicket;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ParkingTicketApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
